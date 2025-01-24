cdbf3b4d0a763b28e5ce9f4b531cee81ce46cd11
Adebayo Olaonipekun <pekunmi@live.com>
2025-01-24 15:32:46 +0000

    Update API with entities

--- api.py
+++ api.py
@@ -40,6 +40,41 @@ def predict(data: InputData):
     }
 
 
+# Endpoint for sex values
+@app.get("/api/entities/sex")
+def get_sex_labels():
+    return {
+        "sex": [
+            {"value": 1, "label": "Male"},
+            {"value": 0, "label": "Female"}
+        ]
+    }
+
+
+# Endpoint for smoker values
+@app.get("/api/entities/smoker")
+def get_smoker_labels():
+    return {
+        "smoker": [
+            {"value": 1, "label": "Yes"},
+            {"value": 0, "label": "No"}
+        ]
+    }
+
+
+# Endpoint for region values
+@app.get("/api/entities/region")
+def get_region_labels():
+    return {
+        "region": [
+            {"columns": [1, 0, 0], "label": "Northwest"},
+            {"columns": [0, 1, 0], "label": "Southeast"},
+            {"columns": [0, 0, 1], "label": "Southwest"},
+            {"columns": [0, 0, 0], "label": "Northeast"}
+        ]
+    }
+
+
 # Run with: uvicorn app:app --reload
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
